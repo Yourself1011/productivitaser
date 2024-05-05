62ed4d45c3d0aae250c02b849a2d06b8f9e5acd5
jeffrey-zang <jeffrey.zang.07@gmail.com>
2024-05-05 03:27:05 -0400

    IF ITS MY FINAL ALBUM

--- entrypoints/options/App.tsx
+++ entrypoints/options/App.tsx
@@ -5,62 +5,68 @@ import Fuse from "fuse.js";
 import { LuPencil } from "react-icons/lu";
 import { FaTrashCan } from "react-icons/fa6";
 import { FaCheck } from "react-icons/fa";
-import { useAutoAnimate } from '@formkit/auto-animate/react'
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { website } from "../types";
+import { AiOutlineNodeIndex } from "react-icons/ai";
+import { IoMdSearch } from "react-icons/io";
 
 export default function () {
-    const [websites, setWebsites] = useImmer<website[]>([]);
-    const [searchString, setSearchString] = useState("");
-    const [status, setStatus] = useState(<span className="text-slate-700">• No Changes</span>);
+  const [websites, setWebsites] = useImmer<website[]>([]);
+  const [searchString, setSearchString] = useState("");
+  const [status, setStatus] = useState(
+    <span className="text-slate-700">• No Changes</span>
+  );
 
-    const [websitesAnimationElement, enable] = useAutoAnimate();
+  const [websitesAnimationElement, enable] = useAutoAnimate();
 
-    function search(sites: website[]) {
-        const options = {
-            includeScore: false,
-            includeMatches: true,
-            threshold: 0.2,
-            keys: ["name"],
-        };
-        const fuse = new Fuse(sites, options);
-        if (searchString.length === 0) return websites;
+  function search(sites: website[]) {
+    const options = {
+      includeScore: false,
+      includeMatches: true,
+      threshold: 0.2,
+      keys: ["name"],
+    };
+    const fuse = new Fuse(sites, options);
+    if (searchString.length === 0) return websites;
 
-        const results = fuse.search(searchString);
-        return results.map((results) => results.item);
-    }
+    const results = fuse.search(searchString);
+    return results.map((results) => results.item);
+  }
 
-    async function save(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        setStatus(<span className="text-yellow-700">• Saving...</span>);
-        await storage.setItem("local:websites", websites);
-        setStatus(<span className="text-green-700">• All Changes Saved</span>);
-    }
+  async function save(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setStatus(<span className="text-yellow-700">• Saving...</span>);
+    await storage.setItem("local:websites", websites);
+    setStatus(<span className="text-green-700">• All Changes Saved</span>);
+  }
 
-    useEffect(() => {
-        (async () => {
-            const fetchedWebsites = (await storage.getItem("local:websites")) as website[];
-            if (fetchedWebsites) setWebsites(fetchedWebsites);
-            else setWebsites([]);
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      const fetchedWebsites = (await storage.getItem(
+        "local:websites"
+      )) as website[];
+      if (fetchedWebsites) setWebsites(fetchedWebsites);
+      else setWebsites([]);
+    })();
+  }, []);
 
   return (
     <div className="p-16">
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-3xl mb-4">Blocked Websites</h1>
-        <div>
-          <span className='text-base'>{status}</span>
+        <div className='relative'>
+          <span className="text-base">{status}</span>
           <input
             placeholder="Search..."
             className="py-2 px-2 ml-2 bg-gray-100 border border-gray-200 rounded outline-none hover:bg-gray-200 hover:border-gray-300 focus:border-gray-400 text-base"
             value={searchString}
             onFocus={() => enable(false)}
             onBlur={() => enable(true)}
-            
             onChange={(e: ChangeEvent<HTMLInputElement>) => {
               setSearchString(e.target.value);
             }}
           ></input>
+          <div className='absolute right-2 top-3'><IoMdSearch size={20} /></div>
         </div>
       </div>
 
@@ -78,24 +84,26 @@ export default function () {
                       ...draftWebsites[index],
                       name: e.target.value,
                     };
-                  })
-                  setStatus(<span className='text-yellow-500'>• Unsaved Changes</span>)
-                  }
-                }
+                  });
+                  setStatus(
+                    <span className="text-yellow-500">• Unsaved Changes</span>
+                  );
+                }}
               />
               <input
                 className="w-full mb-4 outline-none bg-transparent"
                 value={website.description}
-                onChange={(e) =>{
+                onChange={(e) => {
                   setWebsites((draftWebsites) => {
                     draftWebsites[index] = {
                       ...draftWebsites[index],
                       description: e.target.value,
                     };
-                  })
-                  setStatus(<span className='text-yellow-500'>• Unsaved Changes</span>)
-                  }
-                }
+                  });
+                  setStatus(
+                    <span className="text-yellow-500">• Unsaved Changes</span>
+                  );
+                }}
               />
               <p>Visited {website.visits} times</p>
               <input
@@ -103,39 +111,43 @@ export default function () {
                 value={website.url}
                 id={`url-${index.toString()}`}
                 placeholder="https://example.com"
-                onChange={(e) =>{
-                  if (e.target.value !== '') {
+                onChange={(e) => {
+                  if (e.target.value !== "") {
                     setWebsites((draftWebsites) => {
                       draftWebsites[index] = {
                         ...draftWebsites[index],
                         url: e.target.value,
                       };
-                    })
+                    });
                   }
-                  setStatus(<span className='text-yellow-500'>• Unsaved Changes</span>)
-                }
-                }
+                  setStatus(
+                    <span className="text-yellow-500">• Unsaved Changes</span>
+                  );
+                }}
                 required
               />
               <div className="flex items-center gap-2 mt-4">
-                <button 
+                <button
                   className="w-1/2 flex items-center justify-center gap-2"
-                  onClick={
-                    () => {
-                      document.getElementById(`url-${index.toString()}`)?.focus();
-                    }
-                  }
-                ><LuPencil /> Edit</button>
-                <button className="w-1/2 border border-slate-800 bg-white text-slate-800 flex items-center justify-center gap-2" onClick={
-                  () => {
-                    console.log('jonald')
-                    setStatus(<span className='text-yellow-500'>• Unsaved Changes</span>)
-                    console.log('jonald')
+                  onClick={() => {
+                    document.getElementById(`url-${index.toString()}`)?.focus();
+                  }}
+                >
+                  <LuPencil /> Edit
+                </button>
+                <button
+                  className="w-1/2 contrast flex items-center justify-center gap-2"
+                  onClick={() => {
+                    console.log("jonald");
+                    setStatus(
+                      <span className="text-yellow-500">• Unsaved Changes</span>
+                    );
+                    console.log("jonald");
                     setWebsites((draftWebsites) => {
                       draftWebsites.splice(index, 1);
                     });
-                  }
-                }>
+                  }}
+                >
                   <FaTrashCan /> Delete
                 </button>
               </div>
@@ -143,7 +155,7 @@ export default function () {
           ))}
           <button
             type="button"
-            className='shadow-lg h-[173px] border border-slate-100 bg-white p-4 rounded-2xl text-black text-xl hover:bg-neutral-50 transition-colors'
+            className="shadow-lg h-[173px] border border-slate-100 bg-white p-4 rounded-2xl text-black text-xl hover:bg-neutral-50 transition-colors"
             onClick={() => {
               setWebsites([
                 ...websites,
@@ -151,53 +163,58 @@ export default function () {
                   name: "New website",
                   url: "",
                   description: "Description",
-                  visits: 0
+                  visits: 0,
                 },
               ]);
-              setStatus(<span className='text-yellow-500'>• Unsaved Changes</span>)
+              setStatus(
+                <span className="text-yellow-500">• Unsaved Changes</span>
+              );
             }}
           >
             + Add
           </button>
         </div>
-        <button
-          type="submit"
-          className="mt-4 ml-2 border border-slate-800 bg-white text-slate-800 flex items-center justify-center gap-2"
-        >
-          <FaCheck /> Save
-        </button>
-      </form>
 
-      <button
-        onClick={async () => {
-          // console.log("among us");
-          try {
-            const device = await navigator.bluetooth.requestDevice({
-              filters: [{ name: "HC-05" }],
-              // acceptAllDevices: true,
-            });
+        <div className="flex items-center gap-2 mt-4">
+          <button
+            type="submit"
+            className="contrast flex items-center justify-center gap-2"
+          >
+            <FaCheck /> Save
+          </button>
+          <button
+            className="flex items-center justify-center gap-2"
+            onClick={async () => {
+              // console.log("among us");
+              try {
+                const device = await navigator.bluetooth.requestDevice({
+                  filters: [{ name: "HC-05" }],
+                  // acceptAllDevices: true,
+                });
 
-                        const server = await device.gatt?.connect();
-                        console.log(device, server);
-                    } catch (e: any) {
-                        console.log(e.message);
-                    }
+                const server = await device.gatt?.connect();
+                console.log(device, server);
+              } catch (e: any) {
+                console.log(e.message);
+              }
 
-                    const port = await navigator.serial.requestPort();
+              const port = await navigator.serial.requestPort();
 
-                    console.log(port);
-                    await port.open({ baudRate: 38400 });
+              console.log(port);
+              await port.open({ baudRate: 38400 });
 
-                    console.log("opened");
-                    const writer = port.writable?.getWriter();
+              console.log("opened");
+              const writer = port.writable?.getWriter();
 
-                    await writer?.write(new Uint8Array([1]));
-                    writer?.releaseLock();
-                    console.log("written");
-                }}
-            >
-                Connect
-            </button>
+              await writer?.write(new Uint8Array([1]));
+              writer?.releaseLock();
+              console.log("written");
+            }}
+          >
+            <AiOutlineNodeIndex /> Connect
+          </button>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
