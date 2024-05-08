85f0eb2b685b6e92c491727f3378012b43f3d6a6
jeffrey-zang <jeffrey.zang.07@gmail.com>
2024-05-08 12:54:04 -0400

    clean up ui code

--- entrypoints/options/Options.tsx
+++ entrypoints/options/Options.tsx
@@ -1,25 +1,27 @@
 import { useState, useEffect, ChangeEvent } from "react";
-import { storage } from "wxt/storage";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { useImmer } from "use-immer";
+import { storage } from "wxt/storage";
 import Fuse from "fuse.js";
+
+import { website } from "../types";
+
 import { FaTrashCan } from "react-icons/fa6";
 import { FaCheck } from "react-icons/fa";
-import { useAutoAnimate } from "@formkit/auto-animate/react";
-import { website } from "../types";
 import { AiOutlineNodeIndex } from "react-icons/ai";
 import { IoMdSearch } from "react-icons/io";
 import { FaGithub } from "react-icons/fa";
 import { SiDevpost } from "react-icons/si";
 
-export default function () {
+const Options = () => {
   const [websites, setWebsites] = useImmer<website[]>([]);
-  const [searchString, setSearchString] = useState("");
-  const [status, setStatus] = useState(
-    <span className="text-slate-700">• No Changes</span>,
+  const [searchString, setSearchString] = useState<string>("");
+  const [status, setStatus] = useState<"none" | "saving" | "unsaved" | "saved">(
+    "none"
   );
   const [serial, setSerial] = useState<boolean>(false);
 
-  const [websitesAnimationElement, enable] = useAutoAnimate();
+  const [websitesAnimationElement, enableAnimation] = useAutoAnimate();
 
   function search(sites: website[]) {
     const options = {
@@ -37,15 +39,28 @@ export default function () {
 
   async function save(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    setStatus(<span className="text-yellow-700">• Saving...</span>);
     await storage.setItem("local:websites", websites);
-    setStatus(<span className="text-green-700">• All Changes Saved</span>);
+    setStatus("saved");
   }
+
+  const updateDraft = (
+    e: ChangeEvent<HTMLInputElement>,
+    index: number,
+    updatedProperty: keyof website
+  ) => {
+    setWebsites((draftWebsites) => {
+      draftWebsites[index] = {
+        ...draftWebsites[index],
+        [updatedProperty]: e.target.value,
+      };
+    });
+    setStatus("unsaved");
+  };
 
   useEffect(() => {
     (async () => {
       const fetchedWebsites = (await storage.getItem(
-        "local:websites",
+        "local:websites"
       )) as website[];
       if (fetchedWebsites) setWebsites(fetchedWebsites);
       else setWebsites([]);
@@ -57,13 +72,32 @@ export default function () {
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-3xl mb-4">Blocked Websites</h1>
         <div className="relative">
-          <span className="text-base">{status}</span>
+          <span className={`text-base bg-[#fcfcfc] transition-colors duration-300
+            ${status === "none" && "text-slate-700"}
+            ${status === "saving" && "text-yellow-700"}
+            ${status === "saved" && "text-green-700"}
+            ${status === "unsaved" && "text-yellow-500"}
+          `}>
+            {status === "none" && (
+              <span>No Changes</span>
+            )}
+            {status === "saving" && (
+              <span>Saving...</span>
+            )}
+            {status === "saved" && (
+              <span>Saved</span>
+            )}
+            {status === "unsaved" && (
+              <span>Unsaved Changes</span>
+            )}
+            {' •'}
+          </span>
           <input
             placeholder="Search..."
             className="py-2 px-2 ml-4 bg-gray-100 border border-gray-200 rounded outline-none hover:bg-gray-200 hover:border-gray-300 focus:border-gray-400 text-base"
             value={searchString}
-            onFocus={() => enable(false)}
-            onBlur={() => enable(true)}
+            onFocus={() => enableAnimation(false)}
+            onBlur={() => enableAnimation(true)}
             onChange={(e: ChangeEvent<HTMLInputElement>) => {
               setSearchString(e.target.value);
             }}
@@ -75,7 +109,10 @@ export default function () {
       </div>
 
       <form onSubmit={save}>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" ref={websitesAnimationElement}>
+        <div
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
+          ref={websitesAnimationElement}
+        >
           {search(websites).map((website: website, index: number) => (
             <div className="shadow-lg !h-full border border-slate-100 bg-white p-4 rounded-2xl">
               <div className="flex items-center justify-between gap-2">
@@ -83,19 +120,7 @@ export default function () {
                   className="text-xl font-bold w-full outline-none bg-transparent"
                   value={website.name}
                   key={`input-${index.toString()}`}
-                  onChange={(e) => {
-                    setWebsites((draftWebsites) => {
-                      draftWebsites[index] = {
-                        ...draftWebsites[index],
-                        name: e.target.value,
-                      };
-                    });
-                    setStatus(
-                      <span className="text-yellow-500">
-                        • Unsaved Changes
-                      </span>,
-                    );
-                  }}
+                  onChange={(e) => updateDraft(e, index, "name")}
                 />
                 {website.url.length > 0 && (
                   <img
@@ -108,17 +133,7 @@ export default function () {
               <input
                 className="w-full mb-4 outline-none bg-transparent text-base"
                 value={website.description}
-                onChange={(e) => {
-                  setWebsites((draftWebsites) => {
-                    draftWebsites[index] = {
-                      ...draftWebsites[index],
-                      description: e.target.value,
-                    };
-                  });
-                  setStatus(
-                    <span className="text-yellow-500">• Unsaved Changes</span>,
-                  );
-                }}
+                onChange={(e) => updateDraft(e, index, "description")}
               />
               <div className="flex items-center gap-2 mt-2 justify-between">
                 <input
@@ -126,28 +141,14 @@ export default function () {
                   value={website.url}
                   id={`url-${index.toString()}`}
                   placeholder="Keyword, URL or regex"
-                  onChange={(e) => {
-                    setWebsites((draftWebsites) => {
-                      draftWebsites[index] = {
-                        ...draftWebsites[index],
-                        url: e.target.value,
-                      };
-                    });
-                    setStatus(
-                      <span className="text-yellow-500">• Unsaved Changes</span>,
-                    );
-                  }}
+                  onChange={(e) => updateDraft(e, index, "url")}
                   required
                 />
                 <span
                   className="text-slate-500 opacity-50 hover:opacity-100 transition-opacity cursor-pointer"
                   onClick={() => {
-                    enable(false);
-                    setStatus(
-                      <span className="text-yellow-500">
-                        • Unsaved Changes
-                      </span>,
-                    );
+                    enableAnimation(false);
+                    setStatus("unsaved");
                     setWebsites((draftWebsites) => {
                       draftWebsites.splice(index, 1);
                     });
@@ -156,14 +157,16 @@ export default function () {
                   <FaTrashCan />
                 </span>
               </div>
-              <p className="text-xs text-slate-500">Visited {website.visits} times</p>
+              <p className="text-xs text-slate-500">
+                Visited {website.visits} times
+              </p>
             </div>
           ))}
           <button
             type="button"
             className="shadow-lg min-h-[160px] border border-slate-100 bg-white p-4 rounded-2xl text-black text-xl hover:bg-neutral-50 transition-colors h-full"
             onClick={() => {
-              enable(true);
+              enableAnimation(true);
               setWebsites([
                 ...websites,
                 {
@@ -173,9 +176,7 @@ export default function () {
                   visits: 0,
                 },
               ]);
-              setStatus(
-                <span className="text-yellow-500">• Unsaved Changes</span>,
-              );
+              setStatus("unsaved");
             }}
           >
             + Add
@@ -194,23 +195,23 @@ export default function () {
 
       <div>
         <h1 className="mt-8 text-2xl">Serial</h1>
-        <p>
-          {serial ? (
-            <>Connected!</>
-          ) : (<>No serial connected, yet.</>)}
-        </p>
+        <p>{serial ? <>Connected!</> : <>No serial connected, yet.</>}</p>
       </div>
-      <button 
+      <button
         className="flex contrast items-center justify-center gap-2 mt-4"
         onClick={async () => {
           const port = await navigator.serial.requestPort();
 
           await port.open({ baudRate: 9600 });
-          console.log(JSON.stringify(port, function(key, value) { return value === undefined ? "undefined" : value }))
+          console.log(
+            JSON.stringify(port, function (key, value) {
+              return value === undefined ? "undefined" : value;
+            })
+          );
           setSerial(true);
-          
+
           const writer = port.writable?.getWriter();
-          
+
           await writer?.write(new Uint8Array([1]));
           writer?.releaseLock();
         }}
@@ -218,13 +219,27 @@ export default function () {
         <AiOutlineNodeIndex /> Connect
       </button>
 
-      <div className='absolute bottom-0 border-t border-t-neutral-200 bg-white left-0 w-full flex justify-between p-6 text-base'>
-        <p className='flex gap-2 items-center'>Copyright &#169; 2024</p>
-        <div className='flex items-center gap-8'>
-            <a className='flex gap-2 items-center underline' href='https://github.com/Yourself1011/productivity-taser' target='_blank'><FaGithub /> GitHub</a>
-            <a className='flex gap-2 items-center underline' href='https://devpost.com/software/lostazer' target='_blank'><SiDevpost /> Devpost</a>
+      <div className="absolute bottom-0 border-t border-t-neutral-200 bg-white left-0 w-full flex justify-between p-6 text-base">
+        <p className="flex gap-2 items-center">Copyright &#169; 2024</p>
+        <div className="flex items-center gap-8">
+          <a
+            className="flex gap-2 items-center underline"
+            href="https://github.com/Yourself1011/productivity-taser"
+            target="_blank"
+          >
+            <FaGithub /> GitHub
+          </a>
+          <a
+            className="flex gap-2 items-center underline"
+            href="https://devpost.com/software/lostazer"
+            target="_blank"
+          >
+            <SiDevpost /> Devpost
+          </a>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Options;
--- entrypoints/options/main.tsx
+++ entrypoints/options/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import Options from "./Options.tsx";
 import "../popup/style.css";
 
 ReactDOM.createRoot(document.getElementById("options")!).render(
   <React.StrictMode>
-    <App />
+    <Options />
   </React.StrictMode>,
 );
